7efddbbbed5d64832746a1e92a71faaf79177dcd
yuchen li <yuchenli@yuchens-MBP.fios-router.home>
2020-10-17 19:30:25 -0400

    update

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Swipe'),
+      home: MyHomePage(title: 'Swipe Page'),
     );
   }
 }
@@ -29,14 +30,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
+  int removed = 0;
 
-
-  Person user1;
-  Person user2;
-  Person user3;
+  Person user1, user2, user3, user4, user5, user6;
   List<Person> people;
-  List<Person> userMatchedWith;
-  List<Person> matchedWithUser;
 
   _MyHomePageState() {
     user1 = Person('user1.jpg', 'Sarah', 'vegan', 'democrat', 'education',
@@ -45,27 +42,28 @@ class _MyHomePageState extends State<MyHomePage> {
         'democrat', 'Get to know me!');
     user3 = Person('user3.jpg', 'Tom', 'republican', 'pro-life', 'education',
         'Swipe right if you want to have a nice chat!');
-    people = [user1, user2, user3];
-    userMatchedWith = [];
-    matchedWithUser = [];
+    user4 = Person('user1.jpg', 'Annie', 'healthcare', '2020 elections',
+        'education', 'Looking for a good conversation!');
+    user5 = Person('user2.jpg', 'Markus', 'christian', 'sustainability',
+        'vegan', 'Just chattin');
+    user6 = Person('user3.jpg', 'David', 'republican', 'pro-life', 'AI ethics',
+        'Covid has not helped my social life');
+    people = [user1, user2, user3, user4, user5, user6];
   }
-
-  void _onNavTapped(int index) {
+  void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     CardController controller;
-
-    Container swipe = new Container(
+    Container swipe = Container(
       height: MediaQuery.of(context).size.height * 2,
       child: Stack(
         children: <Widget>[
-          new Card(
+          Card(
             child: Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -76,78 +74,156 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
           ),
-          new Card(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text("You and ${user3.name} were a match!",
-                      style: TextStyle(fontSize: 25)),
-                  Text("Chat with them at zoom.com/xyz!",
-                      style: TextStyle(fontSize: 25)),
-                  FlatButton(
-                    child: Text(
-                      'Cancel',
-                      style: TextStyle(fontSize: 20.0),
-                    ),
-                    color: Colors.green,
-                    textColor: Colors.white,
-                    onPressed: () {}, //change this to make it do something
-                  ),
-                ],
-              ),
-            ),
-          ),
-          new TinderSwapCard(
-            swipeUp: true,
-            swipeDown: true,
-            orientation: AmassOrientation.BOTTOM,
+          TinderSwapCard(
             totalNum: people.length,
             stackNum: 3,
-            swipeEdge: 5.0,
             maxWidth: MediaQuery.of(context).size.width * 1,
-            maxHeight: MediaQuery.of(context).size.width * 3,
+            maxHeight: MediaQuery.of(context).size.width * 1.4,
             minWidth: MediaQuery.of(context).size.width * 0.95,
             minHeight: MediaQuery.of(context).size.width * 0.95,
             cardBuilder: (context, index) => Card(
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  Image.asset('${people[index].image}'),
-                  Text(people[index].name, style: TextStyle(fontSize: 25)),
-                  Chip(label: Text(people[index].tag1)),
-                  Chip(label: Text(people[index].tag2)),
-                  Chip(label: Text(people[index].tag3)),
-                  Text(people[index].bioText),
-                ],
+              child: Container(
+                padding: const EdgeInsets.only(
+                    bottom: 20, top: 20, left: 20, right: 20),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Container(
+                      width: 250.0,
+                      height: 250.0,
+                      alignment: Alignment.center,
+                      decoration: new BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('${people[index].image}'),
+                            fit: BoxFit.fill),
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            RichText(
+                              text: TextSpan(
+                                  style: DefaultTextStyle.of(context).style,
+                                  children: <TextSpan>[
+                                    TextSpan(
+                                        text: people[index].name,
+                                        style: TextStyle(
+                                          fontFamily: 'Raleway',
+                                          fontSize: 25.0,
+                                          fontWeight: FontWeight.bold,
+                                        )),
+                                  ]),
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(people[index].bioText),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Chip(label: Text(people[index].tag1)),
+                            Chip(label: Text(people[index].tag2)),
+                            Chip(label: Text(people[index].tag3)),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
             cardController: controller = CardController(),
             swipeUpdateCallback: (DragUpdateDetails details, Alignment align) {
               /// Get swiping card's alignment
               if (align.x < 0) {
-                //Card is LEFT swiping
-              } else if (align.x > 0) {
-                //Card is RIGHT swiping
-              }
+              } else if (align.x > 0) {}
             },
             swipeCompleteCallback:
-              (CardSwipeOrientation orientation, int index) {
-              /// Get orientation & index of swiped card!
-              },
+                (CardSwipeOrientation orientation, int index) {
+              if (orientation == CardSwipeOrientation.RIGHT) {
+                if (!user.userMatchedWith.contains(people[index]))
+                  user.userMatchedWith.add(people[index]);
+              } else {
+                setState(() {
+                  user.userMatchedWith.remove(people[index]);
+                  people.removeAt(index);
+                });
+              }
+            },
           ),
         ],
       ),
     );
-
     List<Widget> _pages = <Widget>[
-      Text('Matches but change this to be a widget'),
-      swipe,
-      Text('Profile but change this to be a widget')
+      Container(
+        margin: EdgeInsets.all(100.0),
+        child: ListView.separated(
+          padding: const EdgeInsets.all(8),
+          itemCount: user.userMatchedWith.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              child: Container(
+                height: 50,
+                color: Colors.amber[300],
+                child:
+                    Center(child: Text('${user.userMatchedWith[index].name}')),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          MatchInfo(user.userMatchedWith[index].name)),
+                );
+              },
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) =>
+              const Divider(),
+        ),
+      ),
+      Center(child: swipe),
+      Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Container(
+                height: 200.0,
+                color: Colors.orange,
+                child: Center(
+                  child: Image.asset('user1.jpg'),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.white,
+                  child: Center(
+                    child: Text('Introduction here'),
+                  ),
+                ),
+              )
+            ],
+          ),
+          // Profile image
+          Positioned(
+            top: 150.0,
+            child: Container(
+              height: 100.0,
+              width: 100.0,
+              decoration:
+                  BoxDecoration(shape: BoxShape.circle, color: Colors.green),
+            ),
+          )
+        ],
+      )
     ];
 
     return new Scaffold(
-      body: new Center(child: _pages.elementAt(_selectedIndex)),
+      body: _pages.elementAt(_selectedIndex),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -164,7 +240,41 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
         currentIndex: _selectedIndex,
-        onTap: _onNavTapped,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class MatchInfo extends StatelessWidget {
+  @override
+  String _matcher;
+  MatchInfo(this._matcher);
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("You are Matched!"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("You and $_matcher were a match!",
+                style: TextStyle(fontSize: 25)),
+            Text("Chat with them at zoom.com/xyz!",
+                style: TextStyle(fontSize: 25)),
+            FlatButton(
+              child: Text(
+                'Cancel',
+                style: TextStyle(fontSize: 20.0),
+              ),
+              color: Colors.green,
+              textColor: Colors.white,
+              onPressed: () {}, //change this to make it do something
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -177,7 +287,10 @@ class Person {
   String tag2;
   String tag3;
   String bioText;
+  List<Person> userMatchedWith = [];
 
   Person(this.image, this.name, this.tag1, this.tag2, this.tag3,
       this.bioText); //constructor
 }
+
+Person user = new Person(null, 'Myself', '', '', '', '');
